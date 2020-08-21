0676469a5e7c942a6db7366089140f7a52a0167d
Jayabal Somasundaram <jayabal.somasundram@solitontech.com>
2020-08-21 13:08:07 +0530

    Added project file.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Added the files to repo");
+        }
+    }
+}
