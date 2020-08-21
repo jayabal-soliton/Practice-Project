0f7403d27dbe3ae5ba4c5279bc80e9602e7de3fb
Jayabal-Somasundaram <jayabal.somasundram@solitontech.com>
2020-08-21 18:48:14 +0530

    Committed after rebase

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,8 +7,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             int number = 10;
+            string fullName = "Jayabal Somasundaram";
             Console.WriteLine("My first commit in git");
             Console.WriteLine("The Number is = {0}",number);
+            Console.WriteLine("Full Name = " + fullName);
         }
     }
 }
